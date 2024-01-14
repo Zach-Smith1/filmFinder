95092794ca3e3c042abcfc043bbc21d5ba321355
Zach Smith <zachsmithglobal@icloud.com>
2024-01-13 17:04:23 -0800

    server update

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-    express.static(path.join(__dirname, 'dist'))(req, res, next);
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
